31e236e71ca216c642160aa965303257ea86b81c
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-03-24 20:02:39 +0000

    added more files

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,8 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -11,6 +13,15 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
          
     
 # Create your views here.
