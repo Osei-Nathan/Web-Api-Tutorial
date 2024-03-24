89ecd1df210903d4cd2611eb33ff6bc70b4eb9cc
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-03-24 19:39:01 +0000

    Added new features

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -16,6 +19,9 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ['created']
+
+owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+highlighted = models.TextField()
 
 
 # Create your models here.
