23a64805ed0b40ee3d78df2f3ccdda085d81b32e
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-04-03 08:00:14 +0000

    Added response and reverse to views.py

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,6 +6,10 @@ from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
 
 
 class SnippetList(generics.ListCreateAPIView):
