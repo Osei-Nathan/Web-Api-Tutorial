823fe86549cd15172ff60c2cd2a554d46f51edbe
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-04-03 08:16:23 +0000

    Added endpoint for highlighted snippets

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -8,6 +8,18 @@ from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
