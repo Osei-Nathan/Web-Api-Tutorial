7cc7bb7cce4c8f0f6a823912ca44a559db5eea1d
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-04-03 08:44:13 +0000

    Added a url pattern for the snippet highlight

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('', views.api_root),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
